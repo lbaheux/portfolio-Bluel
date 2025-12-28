40a5a03cc174f4024a17b3cc5e1ba67cec2eef04
Louis Baheux <louis.baheux1@gmail.com>
2025-12-28 21:18:08 +0100

    Ajout des filtres dynamiques fonctionnels

--- FrontEnd/assets/main.js
+++ FrontEnd/assets/main.js
@@ -1,8 +1,17 @@
+// Récupération des données de l'API
 async function fetchProjects() {
     const response = await fetch("http://localhost:5678/api/works");
     return response.json();
 }
 
+async function fetchCategories() {
+    const response = await fetch("http://localhost:5678/api/categories");
+    return response.json();
+}
+
+let allProjects = [];
+
+// Affichage de la galerie de projets
 function renderGallery (projects) {
     const galleryElem = document.querySelector(".gallery");
 
@@ -24,5 +33,62 @@ function renderGallery (projects) {
     }
 }
 
+// Affichage des filtres
+function setActiveFilter(button) {
+    const container = document.querySelector(".filters");
+
+    const buttons = container.querySelectorAll("button");
+    for (const btn of buttons) {
+        btn.classList.remove("is-active");
+    }
+    button.classList.add("is-active");
+}
+
+function createFilterButton(label, categoryId, isActive = false) {
+    const button = document.createElement("button");
+    button.type = "button";
+    button.textContent = label;
+    button.dataset.categoryId = categoryId;
+
+    if (isActive) {
+        button.classList.add("is-active");
+    }
+
+    return button;
+}
+
+function renderFilters(categories) {
+    const filtersElem = document.querySelector(".filters");
+    filtersElem.innerHTML = "";
+
+    filtersElem.appendChild(createFilterButton("Tous", "all", true));
+    for (const category of categories) {
+        filtersElem.appendChild(createFilterButton(category.name, category.id));
+    }
+
+    filtersElem.addEventListener("click", (e) => {
+        const btn = e.target;
+        const categoryId = btn.dataset.categoryId;
+        setActiveFilter(btn);
+        
+        if (categoryId == "all") {
+            renderGallery(allProjects);
+        } else {
+            let filteredProjects = [];
+            for (let i = 0; i < allProjects.length; i++) {
+                const project = allProjects[i];
+                if (project.categoryId == categoryId) {
+                    filteredProjects.push(project);
+                }
+            }
+            renderGallery(filteredProjects)
+        }
+    });
+}
+
 const projects = await fetchProjects();
-renderGallery(projects);
+const categories = await fetchCategories();
+
+allProjects = projects;
+renderFilters(categories);
+renderGallery(allProjects);
